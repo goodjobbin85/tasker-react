193cea926831f74aa8ff79352bc7e635d56d105e
Thomas Dubiel <td1729@hotmail.com>
2021-09-02 12:20:51 -0500

    remove unnecesary starter files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tasker</h1> 
+      <h3>The best tool to help organize your life!</h3>
     </div>
   );
 }
